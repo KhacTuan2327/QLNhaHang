081fc92e9db76a0f4acedbee97c0dc5cc5509c70
Hoanganh56 <103049681+Hoanganh56@users.noreply.github.com>
2023-01-08 10:12:36 +0700

    thêm chức năng nhập xuất nguyên liệu trong kho
    TTN6-5 #comment

---
+++ QLNhaHang/Warehouse/ChiTietXuat.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using QLNhaHang.Data;
+using Microsoft.Reporting.WinForms;
+
+namespace QLNhaHang.Warehouse
+{
+    public partial class ChiTietXuat : Form
+    {
+        public ChiTietXuat()
+        {
+            InitializeComponent();
+        }
+        public string soPX;
+        public string ngayXuat;
+        public string maNV;
+        public string hoTen;
+
+        SqlConnection sqlCon = new SqlConnection(Data_Provider.connectionSTR);
+        private void connect(string query)
+        {
+            sqlCon.Open();
+            SqlDataAdapter sqlDa = new SqlDataAdapter(query, sqlCon);
+            DataTable dtb = new DataTable();
+            sqlDa.Fill(dtb);
+            dtgvInChiTietXuat.DataSource = dtb;
+            dtgvInChiTietXuat.AutoGenerateColumns = false;
+            dtgvInChiTietXuat.AllowUserToAddRows = false;
+            dtgvInChiTietXuat.AutoResizeColumns();
+            sqlCon.Close();
+        }
+
+        private void CapNhat(string query)
+        {
+            sqlCon.Open();
+            SqlCommand cmd = new SqlCommand(query + soPX + "'", sqlCon);
+            cmd.ExecuteNonQuery();
+            sqlCon.Close();
+        }
+        private void btIn_Click(object sender, EventArgs e)
+        {
+            InPhieuXuat inPhieuXuat = new InPhieuXuat();
+            inPhieuXuat.sopx = soPX;
+            inPhieuXuat.ShowDialog();
+        }
+
+        private void ChiTietXuat_Load(object sender, EventArgs e)
+        {
+            txtSPX.Text = soPX;
+            txtNgayXuat.Text = ngayXuat;
+            txtMaNV.Text = maNV;
+            txtHoTen.Text = hoTen;
+            string query = "SELECT NGUYENLIEU.MaNL AS N'Mã nguyên liệu', TenNL AS N'Tên nguyên liệu', CHITIETXUAT.SoLuong AS N'Số lượng' FROM NGUYENLIEU, CHITIETXUAT WHERE NGUYENLIEU.MaNL = CHITIETXUAT.MaNL AND CHITIETXUAT.SoPX = '";
+            connect(query + soPX + "'");
+        }
+
+        private void btChinhSua_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DateTime dateEdit = Convert.ToDateTime(ngayXuat);
+                string query = "UPDATE PHIEUXUAT SET SoPX = '" + txtSPX.Text + "', NgayXuat = '" + dateEdit.ToString("yyyy-MM-dd") + "', MaNV = '" + txtMaNV.Text + "' WHERE soPX = '";
+                CapNhat(query);
+                MessageBox.Show("Bạn đã chỉnh sửa thành công phiếu xuất", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch
+            {
+                MessageBox.Show("Chỉnh sửa không thành công", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void txtXoa_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có thục sự muốn xóa phiếu nhập?", "Thông báo", MessageBoxButtons.OKCancel) == System.Windows.Forms.DialogResult.OK)
+            {
+                string query = "DELETE FROM CHITIETXUAT WHERE SoPX = '";
+                CapNhat(query);
+                query = "DELETE FROM PHIEUXUAT WHERE SoPX = '";
+                CapNhat(query);
+                MessageBox.Show("Xóa thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtSPX.Text = string.Empty;
+                txtMaNV.Text = string.Empty;
+                txtNgayXuat.Text = string.Empty;
+                txtHoTen.Text = string.Empty;
+            }
+        }
+    }
+}
---
+++ QLNhaHang/Warehouse/InPhieuNhap.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Reporting.WinForms;
+
+namespace QLNhaHang.Warehouse
+{
+    public partial class InPhieuNhap : Form
+    {
+        public ReportParameterCollection rpc;
+        public DataTable dt;
+        public string sopn;
+        public InPhieuNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void InPhieuNhap_Load(object sender, EventArgs e)
+        {
+
+            //1
+            //reportViewer1.Reset();
+
+            this.reportViewer1.LocalReport.SetParameters(rpc);
+            this.reportViewer1.LocalReport.DataSources.Clear();
+
+            //2
+            reportViewer1.SetDisplayMode(Microsoft.Reporting.WinForms.DisplayMode.Normal);
+            reportViewer1.ZoomMode = Microsoft.Reporting.WinForms.ZoomMode.Percent;
+            reportViewer1.ZoomPercent = 100;
+
+            //3
+            DataSetNhap dataset = new DataSetNhap();
+            dataset.BeginInit();
+            this.reportViewer1.LocalReport.DataSources.Add(new Microsoft.Reporting.WinForms.ReportDataSource("DataSet1", dataset.Tables["DataTable1"]));
+            this.reportViewer1.LocalReport.ReportPath = @"D:\QLNhaHang\Warehouse\PhieuNhap.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            dataset.EndInit();
+
+            //4
+            string connectionString = @"Data Source=DESKTOP-PA4DT27\SQLEXPRESS;Initial Catalog=QuanLyNhaHang;Integrated Security=True";
+
+            //5
+            DataSetNhapTableAdapters.DataTable1TableAdapter nhap = new DataSetNhapTableAdapters.DataTable1TableAdapter();
+            nhap.Connection.ConnectionString = connectionString;
+            nhap.ClearBeforeFill = true;
+            nhap.Fill(dataset.DataTable1, sopn);
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
